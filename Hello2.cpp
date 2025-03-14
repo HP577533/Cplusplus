c0dbe9462ba06fff3c5396a79f6436d4bdec1714
HP577533 <henry.johnson.000@gmail.com>
2025-03-14 16:20:46 +0200

    Added printStuff

--- Hello2.cpp
+++ Hello2.cpp
@@ -8,8 +8,16 @@ void printText(const std::string& text) {
     std::cout << text << std::endl;
 }
 
+void printStuff() {
+    int file_size;
+    int file_size2 = 0;
+    std::cout << file_size << std::endl;
+    std::cout << file_size2 << std::endl;
+}
+
 int main() {
     printHelloWorld();
     printText("Hello World2");
+    printStuff();
     return 0;
 }
