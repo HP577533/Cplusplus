bdfd551e330ab3ebcbb622bafa94de82d8c3153c
HP577533 <henry.johnson.000@gmail.com>
2025-03-14 16:11:18 +0200

    My First C++ Program
    Just making a small program to get me started

---
+++ Hello.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;      // Print Hello, World! and a new line
+    std::cout << "You are the best!" ;              // Print You are the best! without a new line
+    return 0;
+}
