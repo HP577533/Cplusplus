9daf3df628a89e416290669bff8f825b6de9bdf2
HP577533 <henry.johnson.000@gmail.com>
2025-03-14 16:24:29 +0200

    Added swapVlues

--- Hello2.cpp
+++ Hello2.cpp
@@ -15,9 +15,27 @@ void printStuff() {
     std::cout << file_size2 << std::endl;
 }
 
+void swapVlues(){
+    int a = 5;
+    int b = 10;
+    std::cout << "Before swap:" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+    int temp = a;
+    a = b;
+    b = temp;
+    std::cout << "After swap:" << std::endl;
+    std::cout << a << std::endl;
+    std::cout << b << std::endl;
+}
+
 int main() {
     printHelloWorld();
+    std::cout << std::endl;
     printText("Hello World2");
+    std::cout << std::endl;
     printStuff();
+    std::cout << std::endl;
+    swapVlues();
     return 0;
 }
